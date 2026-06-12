f0910c667327f9a1acde91675007fb768598d7d8
Himasha Wijewickrama <himashawije5678@gmail.com>
2026-06-13 00:55:18 +0530

    feat: create AboutPage component to display restaurant information

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import About from "@/components/about";
+
+export const metadata = {
+  title: "About - Relish",
+};
+
+export default function AboutPage() {
+  return (
+    <main>
+      <About />
+    </main>
+  );
+}
