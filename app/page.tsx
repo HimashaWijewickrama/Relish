fc5443e41b9a7ab47250434614a7b070cf4df435
Himasha Wijewickrama <himashawije5678@gmail.com>
2026-06-15 00:19:14 +0530

    feat: add Testimonials component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/about";
 import Hero from "@/components/hero";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
     <>
     <Hero/>
     <About/>
+    <Testimonials/>
     </>
   );
 }
